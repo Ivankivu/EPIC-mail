f089732b2e9dc9ded2ade376fb4e5df4701c6826
Ivankivu <ivankivu@gmail.com>
2019-03-18 10:26:36 +0300

    admin add new group [#164595309]

---
+++ UI/js/main.js
@@ -0,0 +1,63 @@
+function email(evt, emailTab) {
+    // Declare all variables
+    var i, tabcontent, tablinks;
+  
+    // Get all elements with class="tabcontent" and hide them
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the link that opened the tab
+    document.getElementById(emailTab).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
+  $(function() {
+    $( ".tablinks" ).tabs({
+      activate:function(event,ui){
+
+        localStorage.setItem("lastTab",ui.newTab.index() );
+      },
+      active: localStorage.getItem("lastTab") || 0
+    });
+ });
+  
+
+
+// check message nox if empty before sending
+  function required()
+  {
+  var empt = document.form.textarea.value;
+  if (empt === "")
+  {
+  alert("Message empty..");
+  return false;
+  }
+  else 
+  {
+  alert('Message Sent!');
+  return true; 
+  }
+  }
+
+
+
+//login form
+function check(form){
+    if (form.email.value == "admin@andela.ug" && form.pass.value == "andela256"){
+        window.open('admin.html')
+    }
+    if (form.email.value == 'ivan@gmail.com' && form.pass.value == 'uganda'){
+        window.open('user.html')
+    }
+    else{
+        alert("Error Password or Username");
+        window.location.href('index.html');
+    }
+}
