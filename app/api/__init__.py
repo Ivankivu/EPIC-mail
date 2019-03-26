c1cae09b1447c72493ce6872085acba5936e2e04
Ivankivu <ivankivu@gmail.com>
2019-03-26 15:31:06 +0300

    ft-api-v1, add refactor code

--- app/api/__init__.py
+++
@@ -1,8 +0,0 @@
-from app import app
-from app.config import app_config
-
-
-def create_app(config_name):
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
-    return app
--- run.py
+++
@@ -1,7 +0,0 @@
-import os
-from flask import Flask
-from app import app
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5500)
